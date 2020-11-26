2b8e771616a07612aeb7791ba47fe69715bdade7
Riccardo <rickycortef@gmail.com>
2020-11-26 20:38:03 +0100

    Added QuestionarieLog entity

---
+++ db2_projectEJB/ejbModule/it/polimi/db2/entities/QuestionaireLog.java
@@ -0,0 +1,56 @@
+package it.polimi.db2.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="QuestionaireLogs")
+public class QuestionaireLog implements Serializable {
+
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@ManyToOne
+	@JoinColumn(name="id")
+	private User user;
+	
+	@ManyToOne
+	@JoinColumn(name="id")
+	private Product product;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date openDate;
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public Date getOpenDate() {
+		return openDate;
+	}
+	
+
+}
