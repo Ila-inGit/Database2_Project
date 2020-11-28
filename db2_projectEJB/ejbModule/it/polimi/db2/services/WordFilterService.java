83739276a83a55b90d43dd80141e22e70412ab73
Riccardo <rickycortef@gmail.com>
2020-11-28 19:03:26 +0100

    Added blacklist word check

--- db2_projectEJB/ejbModule/it/polimi/db2/services/WordFilterService.java
+++ db2_projectEJB/ejbModule/it/polimi/db2/services/WordFilterService.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.*;
 
+
 @Stateless
 public class WordFilterService {
 	@PersistenceContext(unitName="db2_project")
@@ -19,5 +20,21 @@ public class WordFilterService {
 	 */
 	public List<ForbiddenWord> getNotAllowedWords(){
 		return em.createNamedQuery("ForbiddenWord.findAll", ForbiddenWord.class).getResultList();
+	}
+	
+	
+	/**
+	 * Check if the message contains one or more filtered words
+	 * @param message message to check
+	 * @return true if massage contains one or more not allowed words
+	 */
+	public boolean ConstainsNotAllowedWord(String message) {
+		/* performance goes brrrrrr */
+		for(var  w : getNotAllowedWords()) {
+			if(message.contains(w.getText()))
+				return true;
+		}
+		
+		return false;
 	}
 }
